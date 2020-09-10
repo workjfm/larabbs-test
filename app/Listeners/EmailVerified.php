03240ed2a91019b154f8e0b9a092f3a22ecf8cba
vagrant <vagrant@homestead>
2020-09-10 02:51:25 +0000

    认证后的消息提醒

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+
+class EmailVerified
+{
+    /**
+     * Handle the event.
+     *
+     * @param  Verified  $event
+     * @return void
+     */
+    public function handle(Verified $event)
+    {
+        session()->flash('success', '邮箱验证成功^_^');
+    }
+}
